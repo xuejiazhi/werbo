88e1c48c49cd272374579fdd56482ee081292a30
xuejiazhi <xuejiazhi@gmail.com>
2017-11-20 22:52:09 +0800

    get json config

---
+++ utils/base.go
@@ -0,0 +1,21 @@
+/**
+*@author JEE
+*
+*读取配置
+*/
+package utils
+
+import(
+	// "io/ioutil"
+)
+
+func LoadConfig(filename,item string){
+
+}
+
+
+//readfile
+// func readFile(filename string)(map[string]interface{},bool){
+// 	// bytes, err := ioutil.ReadFile(filename)
+
+// }
